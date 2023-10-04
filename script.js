b5d887169a96f07b921fb2e967600107cdd5a2a1
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-10-05 05:19:27 +0800

    Create private board variable and isEmpty fn

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 //Create module for the game board
 const gameBoardModule = (() => {
+    //Private variable to store the board state
     let board = [[null, null, null],[null, null, null],[null, null, null]];
+
+    //Private function to check if a cell is empty
+    const isEmpty = (row, col) => board[row][col] === null;
 })
