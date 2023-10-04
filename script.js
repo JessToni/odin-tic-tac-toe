18bc797ff77359ac7c05551b2a148f54b90c593e
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-10-05 05:55:35 +0800

    Add public functions to gameBoardModule

--- script.js
+++ script.js
@@ -5,4 +5,35 @@ const gameBoardModule = (() => {
 
     //Private function to check if a cell is empty
     const isEmpty = (row, col) => board[row][col] === null;
-})
+
+    return {
+        //Method for making a move on the board
+        makeMove(row, col, player) {
+            if (isEmpty(row, col)) {
+                board[row][col] = player;
+                return true; //Move was successful
+            }
+            return false; //Move was invalid
+        },
+
+        isFull() {
+            for (let row = 0; row < 3; row++) {
+                for (let col = 0; col < 3; row++) {
+                    if (isEmpty(row, col)) {
+                        return false; //There's an empty cell, the board is not full
+                    }
+                }
+            }
+            return true;
+        },
+
+        checkWinner() {
+            //Implement a logic for finding the winner
+        },
+
+        reset() {
+            board = [[null, null, null],[null, null, null],[null, null, null]];
+
+        },
+    };
+})();
